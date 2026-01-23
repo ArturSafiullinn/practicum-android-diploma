57f2b93ae0e5080ab398cd1e64fa93e7d422fab0
ArturSafiullin <art22061993@gmail.com>
2026-01-23 17:12:33 +0400

    Добавил необходимые зависимости для проекта и внес необходимые правки в AndroidManifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+
+    alias(libs.plugins.ksp)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -17,25 +20,35 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField(type = "String", name = "API_ACCESS_TOKEN", value = "\"${developProperties.apiAccessToken}\"")
+        buildConfigField(
+            type = "String",
+            name = "API_ACCESS_TOKEN",
+            value = "\"${developProperties.apiAccessToken}\""
+        )
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 
     buildFeatures {
         buildConfig = true
+        compose = true
     }
 }
 
@@ -43,16 +56,58 @@ dependencies {
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
 
-    // UI layer libraries
+    // UI (XML)
     implementation(libs.ui.material)
     implementation(libs.ui.constraintLayout)
 
-    // region Unit tests
-    testImplementation(libs.unitTests.junit)
-    // endregion
+    // Navigation (Fragments)
+    implementation(libs.nav.fragmentKtx)
+    implementation(libs.nav.uiKtx)
 
-    // region UI tests
+    // Retrofit + OkHttp
+    implementation(libs.network.retrofit)
+    implementation(libs.network.retrofitGson)
+    implementation(libs.network.okhttp)
+    implementation(libs.network.okhttpLogging)
+
+    // Room (KSP)
+    implementation(libs.db.roomRuntime)
+    implementation(libs.db.roomKtx)
+    ksp(libs.db.roomCompiler)
+
+    // Glide (через kapt)
+    implementation(libs.img.glide)
+    kapt(libs.img.glideCompiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutinesAndroid)
+
+    // Lifecycle
+    implementation(libs.androidX.lifecycleRuntimeKtx)
+    implementation(libs.androidX.lifecycleViewModelKtx)
+
+    // Fragments helpers
+    implementation(libs.androidX.fragmentKtx)
+
+    // Koin
+    implementation(libs.di.koinAndroid)
+    implementation(libs.di.koinCompose)
+
+    // Compose (через BOM)
+    implementation(platform(libs.ui.composeBom))
+    implementation(libs.ui.composeUi)
+    implementation(libs.ui.composeRuntime)
+    implementation(libs.ui.composeUiToolingPreview)
+    implementation(libs.ui.composeMaterial3)
+    implementation(libs.ui.activityCompose)
+
+    // Часто нужно почти сразу (scroll, basic layouts)
+    implementation(libs.ui.composeFoundation)
+
+    debugImplementation(libs.ui.composeUiTooling)
+
+    // Tests
+    testImplementation(libs.unitTests.junit)
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
-    // endregion
 }
