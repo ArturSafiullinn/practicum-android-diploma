244e01e9b8bbac57f132c350057e158171d2bfa9
Denis <d-e-n-2011@yandex.ru>
2026-01-27 16:15:43 +0300

    Добавил сущности для вакансии (domain и ui)

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/models/VacancyDetailUi.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.ui.models
+
+data class VacancyDetailUi(
+    val name: String,
+    val description: String?,
+    val salary: String,
+    val address: String?,
+    val experience: String?,
+    val schedule: String?,
+    val employment: String?,
+    val contactsName: String?,
+    val contactsEmail: String?,
+    val contactsPhone: List<String>?,
+    val employerName: String,
+    val employerLogoLink: String?,
+    val skills: List<String>,
+    val url: String,
+    val industry: String
+)
