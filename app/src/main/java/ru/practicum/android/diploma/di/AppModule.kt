5611b2f24b6748706dad75c5271423088c2dbcb4
Denis <d-e-n-2011@yandex.ru>
2026-01-27 16:17:01 +0300

    Добавил ResourceProvider.kt + di для него

--- app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
@@ -1,8 +1,11 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.util.ResourceProvider
 
 val appModule = module {
+
+    single { ResourceProvider(get()) }
     // View Models
     // Repos
     // Interactors
