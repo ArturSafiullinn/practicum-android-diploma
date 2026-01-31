c58dbcf7525bf14539e52999ab06965e2e0f4f86
Denis <d-e-n-2011@yandex.ru>
2026-02-01 01:25:06 +0300

    Добавил 2 недостающих стейта для экрана вакансии + картинки и строки

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/vacancy/VacancyUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/vacancy/VacancyUiState.kt
@@ -1,8 +1,19 @@
 package ru.practicum.android.diploma.ui.screens.vacancy
 
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.models.VacancyDetailUi
 
 sealed interface VacancyUiState {
-    data object VacancyNotFound : VacancyUiState
+    data object Loading : VacancyUiState
+    data class VacancyNotFound(
+        val imageResId: Int = R.drawable.image_vacancy_not_found,
+        val textResId: Int = R.string.vacancy_not_found_or_deleted
+    ) : VacancyUiState
+
+    data class ServerError(
+        val imageResId: Int = R.drawable.image_vacancy_server_error,
+        val textResId: Int = R.string.server_error
+    ) : VacancyUiState
+
     data class Vacancy(val vacancy: VacancyDetailUi) : VacancyUiState
 }
