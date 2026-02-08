5bd89cef3ae2a5c85d495e85898bc8d9981cc50b
vol55 <akpx99@gmail.com>
2026-02-08 10:58:20 +0500

    detekt changes

--- app/src/main/java/ru/practicum/android/diploma/domain/api/FilterRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FilterRepository.kt
@@ -3,8 +3,7 @@ package ru.practicum.android.diploma.domain.api
 import ru.practicum.android.diploma.domain.models.Area
 import ru.practicum.android.diploma.domain.models.FilterIndustry
 
-
-@Suppress("TooManyFunctions")  // Временное решение
+@Suppress("TooManyFunctions") // Временное решение
 interface FilterRepository {
     suspend fun saveCountry(area: Area)
     suspend fun saveRegion(area: Area?)
