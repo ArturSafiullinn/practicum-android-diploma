50950c3147c64d8bf0090da263504c5913306704
vol55 <akpx99@gmail.com>
2026-02-08 05:50:38 +0500

    remove trailing spaces

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -43,7 +43,7 @@ val dataModule = module {
             networkClient = get()
         )
     }
-    
+
     single<StorageClient<FilterParameters>> {
         PrefsStorageClient(
             context = get(),
